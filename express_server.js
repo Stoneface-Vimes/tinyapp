0033e44b674c97e7ac54b30d38c6411a7d7d336c
Stoneface-Vimes <cliffweir92@gmail.com>
2020-01-20 14:45:19 -0500

    Created a basic server using express: express_server

---
+++ express_server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = 8080; // default port of 8080, redirects to 8000 in vagrant
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xk": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
