55f16a3f8529c009d42d3f97086f67a141f82516
Stoneface-Vimes <cliffweir92@gmail.com>
2020-01-20 18:23:28 -0500

    Created a form that allows a user to input a longurl send that data to the API via a post request Created a route that would render this form when the user visited /urls/new Also created a route to handle the PST request from our form We used the body-parser library to make the POST request body 'human readable' Finally, we generated a random string toserve as our shortURL

--- express_server.js
+++ express_server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const generateRandomString = require("./randomStringGenerator")
 const app = express();
+const bodyParser = require("body-parser")
 const PORT = 8080; // default port of 8080, redirects to 8000 in vagrant
 
+app.use(bodyParser.urlencoded({extended: true}))
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -9,11 +12,25 @@ const urlDatabase = {
   "9sm5xk": "http://www.google.com"
 };
 
+
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); //Log the POST request body to the console
+  res.send("Ok"); //Respond with OK (placeholder response)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+})
+
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+//Checks the user defined router parameter against the urlDatabase and returns the relevant value
+//Since anything following the : will be taken as an route parameter, needs to be the last route checked
+//when working with 'urls/'
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
@@ -30,7 +47,6 @@ app.listen(PORT, () => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
-
 
 //Sending a response with html, that will be displayed as html in browser
 app.get("/hello", (req, res) => {
