b4a28ce5ac910949e5a88b55b420bb42ad4a7434
Stoneface-Vimes <cliffweir92@gmail.com>
2020-01-23 19:06:58 -0500

    Updated bcrypt to work with the version of node that is used for testing. Final commit before going requirements, will try and commit after each change

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,5 +1,6 @@
 const { assert } = require('chai');
-
+//The below line breaks the script when I put getUserByEmail in {}, which is odd since that's
+//how it's given to us. the test crashes and says it's not a function. Assert works fine though
 const getUserByEmail = require("../getUserByEmail")
 
 const testUsers = {
@@ -21,5 +22,9 @@ describe('getUserByEmail', function() {
     const expectedOutput = "userRandomID";
     assert(user === expectedOutput, 'The user equals the expected output')
   });
-  //it('should return undefined when there is no matching email in the database')
+  it('should return undefined when there is no matching email in the database', function(){
+    const user = undefined;
+    const expectedOutput = getUserByEmail("user@example.commmmm", testUsers)
+    assert(user === expectedOutput, 'They should both read undefined as neither are in the database')
+  });
 });
